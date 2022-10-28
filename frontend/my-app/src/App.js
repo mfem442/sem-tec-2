e0ea3c430dcff9677be77ee751b6a9c9b5021a5c
Mafer <A01634135@tec.mx>
2022-10-28 15:41:22 -0500

    Editar LoginPage

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -5,6 +5,9 @@ import Login from './pages/Login';
 import Profile from './pages/Profile';
 import UserPage from './pages/UserPage';
 import MainPage from './pages/MainPage';
+import { NavLink } from "react-router-dom";
+
+
 
 function Topbar(){
   let location = useLocation
@@ -13,9 +16,11 @@ function Topbar(){
     <header>
       <nav className="navbar navbar-expand navbar-dark bg-dark sticky-top" aria-label="Second navbar example">
         <div className="container-fluid">
-          <a  className="nav-link" >
-            <Link to='/'>Semana Tec</Link>
-          </a>
+            <Link className="nav-link" to='/' style={{
+              fontWeight: "bold",
+              textDecoration: 'none',
+              color: "white"
+            }}>Semana Tec</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -23,19 +28,25 @@ function Topbar(){
           <div className="collapse navbar-collapse" id="navbarsExample02">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
-                <a  className="nav-link" >
-                  <Link to='/login'>Login</Link>
-                </a>
+                  <Link className="nav-link" to='/login'
+                  style={{
+                    textDecoration: 'none',
+                    color: 'gray'
+                  }}>Login</Link>
               </li>
               <li className="nav-item">
-                <a  className="nav-link" >
-                  <Link to='/users'>Usuarios</Link>
-                </a>
+                  <Link className="nav-link" to='/users'
+                  style={{
+                    textDecoration: 'none',
+                    color: 'gray'
+                  }}>Usuarios</Link>
               </li>
                 <li className="nav-item">
-                  <a  className="nav-link" >
-                    <Link to='/profile'>Perfil</Link>
-                  </a>
+                    <Link className="nav-link" to='/profile'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'gray'
+                    }}>Perfil</Link>
               </li>
             </ul>
           </div>
--- frontend/my-app/src/pages/Login.js
+++ frontend/my-app/src/pages/Login.js
@@ -1,53 +1,110 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-import { useRef } from 'react';
-import { API_URL } from '../config';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import { API_URL } from "../config";
 
 function Login() {
-    const email = useRef("")
-    const password = useRef("")
-    function submitLogin(e){
-        e.preventdefault();
-        console.log(email.current.value)
-        console.log(password.current.value)
-        fetch(API_URL+"/token", {
-          method: "POST",
-          body: new URLSearchParams({
-            'username':email.current.value,
-            'password': password.current.value,
-          }).then((data) => console.log(data))
-        })
-        alert("Login exitoso")
+	const location = useLocation();
+	const urlParams = new URLSearchParams(location.search);
+	const navigate = useNavigate();
+	const email = useRef("");
+	const password = useRef("");
+	function submitLogin(e) {
+		e.preventdefault();
+		let successful = false;
+		fetch(API_URL + "/token", {
+			method: "POST",
+			body: new URLSearchParams({
+				username: email.current.value,
+				password: password.current.value,
+			}),
+		})
+			.then((data) => {
+				if (data.status === 200) {
+					successful = true;
+				}
+				return data.json();
+			})
+			.then((data) => {
+				if (data.status === 200) {
+					successful = true;
+				}
+				return data.json();
+			})
+			.then((data) => {
+				if (successful) {
+					localStorage.setItem("token", data.access_token);
+					fetch(API_URL + "/users/me/", {
+						headers: {
+							Authorization: "Bearer " + data.access_token,
+						},
+					})
+						.then((data) => {
+							return data.json();
+						})
+						.then((data) => {
+							fetch(
+								"https://api.github.com/users/" +
+									data.github_user
+							)
+								.then((data) => data.json())
+								.then((data) => {
+									localStorage.setItem(
+										"userData",
+										JSON.stringify(data)
+									);
+									setUserData(data);
+									setToken(localStorage.getItem("token"));
+									navigate(urlParams.get("next") || "/");
+								});
+						});
+				} else {
+					throw new Error(data.detail);
+				}
+			})
+			.catch((data) => alert(data));
+	}
+	return (
+		<>
+			<h1> Sign in </h1>
+			<form onSubmit={submitLogin}>
+				<h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-    }
-    return (
-      <>
-      <h1> Sign in </h1>
-      <form onSubmit={submitLogin}>
-    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+				<div className="form-floating">
+					<input
+						ref={email}
+						type="email"
+						className="form-control"
+						id="floatingInput"
+						placeholder="name@example.com"
+					/>
+					<label for="floatingInput">Email address</label>
+				</div>
 
-    <div className="form-floating">
-      <input ref = {email} type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div className="form-floating">
-      <input ref = {password} type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-      <label for="floatingPassword">Password</label>
-    </div>
+				<div className="form-floating">
+					<input
+						ref={password}
+						type="password"
+						className="form-control"
+						id="floatingPassword"
+						placeholder="Password"
+					/>
+					<label for="floatingPassword">Password</label>
+				</div>
 
-    <div className="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"/> Remember me 
-      </label>
-    </div>
-    <Link to = "/">
-      <button type="button" className="btn btn-primary">Sign in</button>
-      </Link>
-  </form>
-      
-      </>
-      
-    );
-  }
+				<div className="checkbox mb-3">
+					<label>
+						<input type="checkbox" value="remember-me" />
+						Remember me
+					</label>
+				</div>
 
-  export default Login;
+				<button className="w-100 btn btn-lg btn-primary" type="submit">
+					Sign in
+				</button>
+			</form>
+		</>
+	);
+}
+
+export default Login;
